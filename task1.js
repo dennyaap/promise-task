3e846c68de62885261179109c2fa4ae9e183a0b5
LisaWebG <alicekeyhey@gmail.com>
2022-07-25 10:24:17 +0300

    empty func

---
+++ task1.js
@@ -0,0 +1,28 @@
+// Реализовать функцию которая должена падать,
+// с ошибкой new Error("timeout")
+// если promise не выполняется за timeout
+function timeoutedPromise(promise, timeout) {
+  return promise;
+}
+
+//реализовать значение с задержкой
+const delayed = (v, timeout) =>
+  new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(v);
+    }, timeout);
+  });
+
+// тесты функции
+(async () => {
+  try {
+    await timeoutedPromise(delayed(1, 1000), 10000);
+    throw new Error('SHOULD THROW');
+  } catch (e) {
+    console.log(e.message);
+  }
+})();
+
+timeoutedPromise(Promise.resolve(1), 1000).then((v) => console.log(v));
+
+timeoutedPromise(Promise.reject(1), 1000).catch((v) => console.log(v));
